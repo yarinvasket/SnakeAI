59b964fafdaad8a2dd5dadea5d77635a715c95fe
vasker <yarinvasket@gmail.com>
2019-04-10 17:13:58 +0300

    modified:   src/SnakeGame.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -2,45 +2,42 @@ import java.util.List;
 
 public class SnakeGame implements Game {
 
+	public SnakeGame() {
+		
+	}
+	
 	@Override
 	public void setInput(List<Float> input) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public List<Float> getOutput() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public float getScore() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public boolean isAlive() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public int getInputAmount() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int getOutputAmount() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	
