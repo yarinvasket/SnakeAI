217b1f4d8ec43aee38457899032dc11f6b280bf2
vasker <yarinvasket@gmail.com>
2019-04-11 17:13:53 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Game snakeGame = new SnakeGame(17);
+		System.out.println("Path to save the population file: ");
+		Scanner scanner = new Scanner(System.in);
+		String filePath = scanner.nextLine();
+		scanner.close();
+		Population population = new Population(1000, snakeGame, filePath);
+		while(true)
+			population.killMutate();
 	}
 
 }
