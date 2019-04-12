62436f00ad2c699153b08162b70145e774b0326c
vasker <yarinvasket@gmail.com>
2019-04-12 15:35:47 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game snakeGame = new SnakeGame(17);
-		System.out.println("Path to save the population file: ");
+		System.out.println("Is there a file saved? (y/n)");
 		Scanner scanner = new Scanner(System.in);
+		String ans = scanner.nextLine();
+		System.out.println("File path: ");
 		String filePath = scanner.nextLine();
 		scanner.close();
-		Population population = new Population(1000, snakeGame, filePath);
-		while(true)
+		Population population = null;
+		if (ans.compareToIgnoreCase("y") == 0)
+			population = (Population) ObjectIO.readObjFromFile(filePath);
+		else
+			population = new Population(1000, snakeGame, filePath);
+		while (true)
 			population.killMutate();
 	}
 
