9f9ee2330ece143e55289c23d2915ec6bbfb7c27
vasker <yarinvasket@gmail.com>
2019-04-17 03:52:57 +0300

    new file:   src/SnakeGame.java

---
+++ src/SnakeGame.java
@@ -0,0 +1,57 @@
+import java.io.Serializable;
+import java.util.List;
+
+public class SnakeGame implements Game, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public SnakeGame(int boardSize) {
+
+	}
+
+	@Override
+	public void setInput(List<Float> input) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Float> getOutput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAlive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getInputAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOutputAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		return (SnakeGame) super.clone();
+	}
+
+}
