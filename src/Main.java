1591d9a27814e9bda4fed334ddf554c87ee84e07
vasker <yarinvasket@gmail.com>
2019-05-02 20:31:25 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Game snakeGame = new SnakeGame(17);
+		Game snakeGame = new SnakeGame();
 		System.out.println("Is there a file saved? (y/n)");
 		Scanner scanner = new Scanner(System.in);
 		String ans = scanner.nextLine();
