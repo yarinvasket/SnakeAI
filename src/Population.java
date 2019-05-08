4b8aa74d067de12195c945fde0114781d9d87bb9
vasker <yarinvasket@gmail.com>
2019-05-08 15:48:23 +0300

    modified:   src/Population.java

--- src/Population.java
+++ src/Population.java
@@ -23,15 +23,10 @@ public class Population implements Serializable {
 		for (int i = 0; i < creatures.size(); i++)
 			scores[i] = testCreature(i);
 		sort(scores);
-
-		try {
-			for (int i = 0; i < creatures.size() / 2; i++) {
-				int num = (int) (i * Math.random());
-				creatures.put(i + creatures.size() / 2, (NeuralNetwork) creatures.get(num).clone());
-				batches.put(i + creatures.size() / 2, (Batch) batches.get(num).clone());
-			}
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
+		for (int i = 0; i < creatures.size() / 2; i++) {
+			int num = (int) (i * Math.random());
+			creatures.put(i + creatures.size() / 2, (NeuralNetwork) creatures.get(num).clone());
+			batches.put(i + creatures.size() / 2, (Batch) batches.get(num).clone());
 		}
 
 		for (int i = 0; i < creatures.size() / 2; i++)
