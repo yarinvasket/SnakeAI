26bd016345b392e1f6e77624101ebb54a97c0f1e
vasker <yarinvasket@gmail.com>
2019-05-13 18:59:45 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Game snakeGame = new SnakeGame();
-		System.out.println("Is there a file saved? (y/n)");
-		Scanner scanner = new Scanner(System.in);
-		String ans = scanner.nextLine();
-		System.out.println("File path: ");
-		String filePath = scanner.nextLine();
-		scanner.close();
+		String filePath = "Population";
 		Population population;
-		if (ans.compareToIgnoreCase("y") == 0)
+		try {
 			population = (Population) ObjectIO.readObjFromFile(filePath);
-		else
+		} catch (FileNotFoundException e) {
+			Game snakeGame = new SnakeGame();
 			population = new Population(1000, snakeGame);
+		}
+		//else
+		//	population = new Population(1000, snakeGame);
 		while (true)
 			population.killMutate();
 	}
