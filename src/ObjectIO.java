7782762c7dad2c7059f1501c2585cbf4a33c3633
vasker <yarinvasket@gmail.com>
2019-05-13 19:10:13 +0300

    modified:   src/ObjectIO.java

--- src/ObjectIO.java
+++ src/ObjectIO.java
@@ -2,11 +2,12 @@ import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.FileNotFoundException;
 
 public class ObjectIO {
 	// copied from:
 	// https://examples.javacodegeeks.com/core-java/io/fileoutputstream/how-to-write-an-object-to-file-in-java/
-	public static void writeObjToFile(Object obj, String filePath) {
+	public static void writeObjToFile(Object obj, String filePath) throws FileNotFoundException {
 		try {
 			FileOutputStream fileOut = new FileOutputStream(filePath);
 			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
@@ -20,7 +21,7 @@ public class ObjectIO {
 
 	// copied from:
 	// https://examples.javacodegeeks.com/core-java/io/file/how-to-read-an-object-from-file-in-java/
-	public static Object readObjFromFile(String filePath) {
+	public static Object readObjFromFile(String filePath) throws FileNotFoundException {
 		try {
 			FileInputStream fileIn = new FileInputStream(filePath);
 			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
